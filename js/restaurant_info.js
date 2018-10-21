984f24d35ecf0fb623f6428f9b89434cc94e6a40
Ogbara Godstar <sengodstar@gmail.com>
2018-10-21 14:53:44 +0100

    fix: Add omitted alt attribute to images

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -49,7 +49,7 @@ initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
-} */
+ } */
 
 /**
  * Get current restaurant from page URL.
@@ -88,7 +88,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.setAttribute('alt', restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
