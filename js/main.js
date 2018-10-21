ee86b96be930442fbe16cf1a5be8c7590ad7748e
Ogbara Godstar <sengodstar@gmail.com>
2018-10-21 08:13:06 +0100

    feat: Add alt attribute to image dynamically

--- js/main.js
+++ js/main.js
@@ -171,6 +171,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = 'Image of Restaurant';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
