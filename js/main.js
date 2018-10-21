4d906d9401f4cedc632865db901a7fa8cad42c01
Ogbara Godstar <sengodstar@gmail.com>
2018-10-21 14:51:28 +0100

    Fix: Add omitted alt attribute to images

--- js/main.js
+++ js/main.js
@@ -171,11 +171,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = 'Image of Restaurant';
+  image.alt = restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
