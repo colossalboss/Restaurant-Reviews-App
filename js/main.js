8c2dcb7117212e93971088be0d08ccd789baebea
Ogbara Godstar <sengodstar@gmail.com>
2018-10-22 01:54:34 +0100

    Update project files

--- js/main.js
+++ js/main.js
@@ -189,6 +189,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role', 'switch');
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.tabIndex = '3';
   li.append(more)
